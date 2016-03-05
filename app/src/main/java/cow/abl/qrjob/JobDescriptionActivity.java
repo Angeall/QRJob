0891d0146a4442ce674364e6b6d9a2310a9fdea5
Angeall <angeal1105@gmail.com>
2016-03-05 15:09:01 +0100

    Many fragments and layouts added

--- app/src/main/java/cow/abl/qrjob/JobDescriptionActivity.java
+++ app/src/main/java/cow/abl/qrjob/JobDescriptionActivity.java
@@ -2,14 +2,22 @@ package cow.abl.qrjob;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+
 public class JobDescriptionActivity extends AppCompatActivity {
+    FloatingActionButton shareButton;
+    FloatingActionButton cvButton;
+    FloatingActionButton saveButton;
+    FloatingActionMenu actionMenu;
+    ListViewCompat listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,14 @@ public class JobDescriptionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_job_description);
         Toolbar toolbar = (Toolbar) findViewById(R.id.job_description_toolbar);
         setSupportActionBar(toolbar);
+        actionMenu = (FloatingActionMenu) findViewById(R.id.job_description_floating_menu);
+        cvButton = (FloatingActionButton) actionMenu.findViewById(R.id.job_cv_button);
+        shareButton = (FloatingActionButton) actionMenu.findViewById(R.id.job_share_button);
+        saveButton = (FloatingActionButton) actionMenu.findViewById(R.id.job_save_button);
+        listView = (ListViewCompat) findViewById(R.id.job_description_list_view);
+        getSupportActionBar().setTitle("##JOB_TITLE");
+
+
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -29,10 +45,20 @@ public class JobDescriptionActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    private class DescriptionAdapter extends ArrayAdapter{
+    private class DescriptionAdapter extends ArrayAdapter<JobDescription>{
 
-        public DescriptionAdapter(Context context, int resource) {
-            super(context, resource);
+        public DescriptionAdapter(Context context, int resource, JobDescription[] objects) {
+            super(context, resource, objects);
+        }
+    }
+
+    private class JobDescription {
+        String title;
+        String description;
+
+        public JobDescription(String title, String description){
+            this.title = title;
+            this.description = description;
         }
     }
 }
